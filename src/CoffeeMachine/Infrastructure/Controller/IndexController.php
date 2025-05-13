1b6db7d8e90535ae2f79c34dbdd5e4dcc79edd8e
Matthieu Braure <braurem@gmail.com>
2025-05-13 18:22:38 +0200

    Basic UI

---
+++ src/CoffeeMachine/Infrastructure/Controller/IndexController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CoffeeMachine\Infrastructure\Controller;
+
+use App\CoffeeMachine\Application\Query\StatusQuery;
+use App\CoffeeMachine\Domain\Entity\CoffeeSize;
+use App\Shared\Infrastructure\QueryBusInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexController extends AbstractController
+{
+    public function __construct(private readonly QuerybusInterface $queryBus){
+
+    }
+
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+        $machine = $this->queryBus->handle(new StatusQuery());
+        return $this->render('index.html.twig', [
+            'machine' => $machine,
+            'coffeeSizes' => CoffeeSize::cases()
+        ]);
+    }
+}
---
+++ src/CoffeeMachine/Infrastructure/Controller/OrderHistoryController.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CoffeeMachine\Infrastructure\Controller;
+
+use App\CoffeeMachine\Application\Query\CompletedOrderQuery;
+use App\CoffeeMachine\Application\Query\StatusQuery;
+use App\CoffeeMachine\Application\View\OrderView;
+use App\CoffeeMachine\Domain\Entity\CoffeeSize;
+use App\Shared\Infrastructure\QueryBusInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class OrderHistoryController extends AbstractController
+{
+    public function __construct(private readonly QuerybusInterface $queryBus){
+
+    }
+
+    #[Route('/order/history', name: 'order_history')]
+    public function index(): Response
+    {
+        $orders = $this->queryBus->handle(new CompletedOrderQuery());
+
+        $ordersByDay = [];
+        /** @var OrderView $order */
+        foreach ($orders as $order) {
+            $day = $order->updatedAt->format('d-m-Y');
+            if (!isset($ordersByDay[$day])) {
+                $ordersByDay[$day] = [];
+            }
+            $ordersByDay[$day][] = $order;
+        }
+        return $this->render('order_history.html.twig', [
+            'ordersByDay' => $ordersByDay,
+        ]);
+    }
+}
